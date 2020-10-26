443e225848509c1c85caceae5b52c587c124e027
Brian Anderson <andersrb@gmail.com>
2020-10-26 06:50:46 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
 }
 
 use std::collections::BTreeMap;
+use float_ord::FloatOrd;
 
 type String = std::string::String;
 type Integer = i64;
@@ -30,13 +31,13 @@ enum ColumnData {
     Integer(Vec<Integer>),
     Float(Vec<Float>),
     String(Vec<String>),
-    Date(Vec<String>),
+    Date(Vec<Date>),
 }
 
 #[derive(Clone)]
 enum UniqueIndex {
     Integer(BTreeMap<Integer, usize>),
-    Float(BTreeMap<Float, usize>),
+    Float(BTreeMap<FloatOrd<Float>, usize>),
     String(BTreeMap<String, usize>),
     Date(BTreeMap<Date, usize>),
 }
@@ -59,29 +60,46 @@ impl Table {
         let mut name_to_idx = BTreeMap::new();
         let mut unique_indexes = BTreeMap::new();
 
-        for (idx, column) in columns.iter().enumerate() {
+        for (column_idx, column) in columns.iter().enumerate() {
             let new_rows = column.len();
             if let Some(rows) = rows {
                 assert_eq!(rows, new_rows);
             }
             rows = Some(new_rows);
-            name_to_idx.insert(column.name.clone(), idx);
+            name_to_idx.insert(column.name.clone(), column_idx);
 
             if column.unique_key {
-                let mut index = unique_indexes.entry(idx).or_insert_with(|| {
-                    match column.data {
-                        ColumnData::Integer(_) => {
-                            UniqueIndex::Integer(BTreeMap::new())
+                let index = match &column.data {
+                    ColumnData::Integer(ref key_column) => {
+                        let mut index = BTreeMap::new();
+                        for (row_idx, key) in key_column.iter().enumerate() {
+                            index.insert(key.clone(), row_idx);
                         }
-                        _ => panic!()
+                        UniqueIndex::Integer(index)
                     }
-                });
-                match &column.data {
-                    ColumnData::Integer(_) => {
-                        panic!()
+                    ColumnData::Float(ref key_column) => {
+                        let mut index = BTreeMap::new();
+                        for (row_idx, key) in key_column.iter().enumerate() {
+                            index.insert(FloatOrd(key.clone()), row_idx);
+                        }
+                        UniqueIndex::Float(index)
                     }
-                    _ => panic!()
-                }
+                    ColumnData::String(ref key_column) => {
+                        let mut index = BTreeMap::new();
+                        for (row_idx, key) in key_column.iter().enumerate() {
+                            index.insert(key.clone(), row_idx);
+                        }
+                        UniqueIndex::String(index)
+                    }
+                    ColumnData::Date(ref key_column) => {
+                        let mut index = BTreeMap::new();
+                        for (row_idx, key) in key_column.iter().enumerate() {
+                            index.insert(key.clone(), row_idx);
+                        }
+                        UniqueIndex::Date(index)
+                    }
+                };
+                unique_indexes.insert(column_idx, index);
             }
         }
 

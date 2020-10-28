87d9f9772bb5f192a66ca0e854e8eab203b70908
Brian Anderson <andersrb@gmail.com>
2020-10-28 06:27:58 +0000

    .

--- src/typecheck.rs
+++ src/typecheck.rs
@@ -1,9 +1,13 @@
-use anyhow::Result;
+use anyhow::{Result, bail};
 use crate::ast::{Schema, Relation};
 use std::collections::BTreeSet;
+use std::collections::BTreeMap;
+
+type TableName = String;
 
 pub fn check(ast: &Schema) -> Result<TypeInfo> {
-    let relation_names = get_relation_names(ast);
+    let relation_names = check_relation_names(ast)?;
+    let relation_deps = check_relation_preds(ast, &relation_names)?;
 
     panic!()
 }
@@ -11,7 +15,7 @@ pub fn check(ast: &Schema) -> Result<TypeInfo> {
 #[derive(Debug)]
 pub struct TypeInfo;
 
-fn get_relation_names(ast: &Schema) -> BTreeSet<String> {
+fn check_relation_names(ast: &Schema) -> Result<BTreeSet<TableName>> {
     let mut set = BTreeSet::new();
     for rel in &ast.rels {
         let name = match rel {
@@ -19,8 +23,38 @@ fn get_relation_names(ast: &Schema) -> BTreeSet<String> {
             Relation::Join(r) => &r.name,
             Relation::View(r) => &r.name,
         };
+        if set.contains(name) {
+            bail!("relation {} appears multiple times", name);
+        }
         let name = name.to_string();
         set.insert(name);
     }
-    set
+
+    Ok(set)
+}
+
+fn check_relation_preds(ast: &Schema, relation_names: &BTreeSet<TableName>)
+                        -> Result<BTreeMap<TableName, BTreeSet<TableName>>> {
+
+    let mut map = BTreeMap::new();
+    
+    for rel in &ast.rels {
+        match rel {
+            Relation::Table(table) => {
+                assert!(!map.contains_key(&table.name));
+                map.insert(table.name.clone(), BTreeSet::new());
+            }
+            Relation::Join(join) => {
+                assert!(!map.contains_key(&join.name));
+                let mut set = BTreeSet::new();
+                set.insert(join.from.clone());
+                for with in &join.with {
+                }
+            }
+            Relation::View(join) => {
+            }
+        }
+    }
+
+    Ok(map)
 }
